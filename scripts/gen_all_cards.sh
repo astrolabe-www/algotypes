c731e7c0b4dfb690f93b9385131e113360292dde
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-11-30 21:41:37 -0300

    script to generate all cards

---
+++ scripts/gen_all_cards.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  echo "specify which type of output [SCREEN | PRINT | TELEGRAM]"
+  exit
+fi
+
+if [ ! -d "out" ] 
+then
+  mkdir -p out
+fi
+
+for ALGO in $(find . -mindepth 2 -maxdepth 2 -type d -name "p5_0x*" | sort)
+do
+  echo $ALGO
+  pushd $ALGO
+  processing-java --sketch=$PWD --run $1
+  mv *png ../../out/
+  popd
+done
